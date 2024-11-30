881b5822f35a0d41020567a60d2eef285468eccd
Bojan Ludajic <bojan.ludajic@gmail.com>
2024-11-30 23:53:49 +0100

    added a custom panel class

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,47 @@
 package com.example;
 
+import com.example.model.ImagePanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+
+
 public class Main {
+    static final int FRAME_WIDTH = 700;
+    static final int FRAME_HEIGHT = 400;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        frame.setVisible(true);
+
+        ImagePanel ip = new ImagePanel();
+        frame.add(ip);
+        ip.setBackground(Color.BLUE);
+        ip.setVisible(false);
+
+        frame.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
+
+        frame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseExited(MouseEvent e) {
+                ip.setVisible(false);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                ip.setVisible(true);
+            }
+        });
+
     }
 }
---
+++ src/main/java/com/example/model/ImagePanel.java
@@ -0,0 +1,31 @@
+package com.example.model;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ImagePanel extends JPanel {
+
+    private int width = 100;
+    private int height = 100;
+
+    public ImagePanel() {
+        this.setSize(width, height);
+    }
+
+    public void growRightLeft(int x, int y) {
+
+    }
+
+    public void growLeftRight(int x, int y) {
+
+    }
+
+    public void growBottomUp(int x, int y) {
+
+    }
+
+    public void growTopDown(int x, int y) {
+
+    }
+
+}
