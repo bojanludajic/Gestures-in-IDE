a9649bab0695fc3aba9a2db267266d1666458782
Bojan Ludajic <bojan.ludajic@gmail.com>
2024-12-01 01:28:03 +0100

    implemented logic for panel sizing

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,27 +7,47 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
-
+import java.util.Random;
 
 
 public class Main {
     static final int FRAME_WIDTH = 700;
-    static final int FRAME_HEIGHT = 400;
+    static final int FRAME_HEIGHT = 500;
+    private static int direction = 1;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        frame.setResizable(false);
         frame.setVisible(true);
 
         ImagePanel ip = new ImagePanel();
         frame.add(ip);
-        ip.setBackground(Color.BLUE);
         ip.setVisible(false);
 
         frame.addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseMoved(MouseEvent e) {
+                int mouseX = e.getX();
+                int mouseY = e.getY();
 
+
+                switch (direction) {
+                    case 1:
+                        ip.growRightLeft(mouseX, mouseY);
+                        break;
+                    case 2:
+                        ip.growLeftRight(mouseX, mouseY);
+                        break;
+                    case 3:
+                        ip.growTopDown(mouseX, mouseY);
+                        break;
+                    case 4:
+                        ip.growBottomUp(mouseX, mouseY);
+                        break;
+                    default:
+                        break;
+                }
             }
         });
 
--- src/main/java/com/example/model/ImagePanel.java
+++ src/main/java/com/example/model/ImagePanel.java
@@ -5,27 +5,72 @@ import java.awt.*;
 
 public class ImagePanel extends JPanel {
 
-    private int width = 100;
-    private int height = 100;
+    private int width = 200;
+    private int height = 200;
 
     public ImagePanel() {
         this.setSize(width, height);
+        this.setBackground(Color.green);
     }
 
+    /*
+        function  for growth f(x) = -(x / 350) + 2
+        f(700) = 0 : right border = quarter size
+        f(350) = 1 : middle = full size
+     */
     public void growRightLeft(int x, int y) {
+        double factor = -((double) 1 / 350) * x + 2;
+        factor = factor > 1 ? 1 : factor;
 
+        width = (int) (50 + 150 * factor);
+        height = (int) (50 + 150 * factor);
+
+        this.setBounds(x - (width / 2), (int) (y - (height * 0.75)), width, height);
     }
 
+    /*
+        function  for growth f(x)= (x - 1) / (350 - 1)
+        f(1) = 0 : left border = quarter size
+        f(350) = 1 : middle of the screen = full size
+     */
     public void growLeftRight(int x, int y) {
+        double factor = ((double) x - 1) / (350 - 1);
+        factor = factor > 1 ? 1 : factor;
 
+        width = (int) (50 + 150 * factor);
+        height = (int) (50 + 150 * factor);
+
+        this.setBounds(x - (width / 2), (int) (y - (height * 0.75)), width, height);
     }
 
+    /*
+        function  for growth f(x)= (500 - y) / 250
+        f(500) = 0 : bottom border = quarter size
+        f(250) = 1 : middle of the screen = full size
+     */
     public void growBottomUp(int x, int y) {
+        double factor = ((double) 500 - y) / 250;
 
+        factor = factor > 1 ? 1 : factor;
+        width = (int) (50 + 150 * factor);
+        height = (int) (50 + 150 * factor);
+
+        this.setBounds(x - (width / 2), (int) (y - (height * 0.75)), width, height);
     }
 
+    /*
+        function  for growth f(x)= (x - 1) / (250 - 1)
+        f(1) = 0 : top border = quarter size
+        f(350) = 1 : middle of the screen = full size
+     */
     public void growTopDown(int x, int y) {
+        double factor = ((double) y - 1) / (250 - 1);
+        factor = factor > 1 ? 1 : factor;
 
+        width = (int) (50 + 150 * factor);
+        height = (int) (50 + 150 * factor);
+
+        this.setBounds(x - (width / 2), (int) (y - (height * 0.75)), width, height);
     }
 
 }
